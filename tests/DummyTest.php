805fa197f01c0f021ae3082c654a23d40af40312
Krzysztof Boduch <boduch.krzysztof@gmail.com>
2019-01-30 22:00:29 +0100

    phpunit test annotation added

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 class DummyTest extends TestCase
 {
     /**
+     * @covers Dummy::__construct
      * @covers Dummy::hello
      */
     public function testHello(): void
